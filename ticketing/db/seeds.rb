33a3cc9df9439911984dd7d285e60f06cefa11ec
hoonloper <yonghoonj98@gmail.com>
2024-02-11 13:18:53 +0900

    Setting: DB Seed 추가

--- ticketing/db/seeds.rb
+++ ticketing/db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+50.times do
+  # 극장 생성
+  theater = Theater.create(name: Faker::Lorem.name, max_audience_count: Faker::Number.number(digits: 1))
+
+  # 영화 생성
+  movie = Movie.create(title: Faker::Lorem.word, director: Faker::Name.name)
+
+  # 극장과 영화에 맞는 상영시간 생성
+  screening = Screening.create(
+      theater: theater,
+      movie: movie,
+      show_date: Faker::Date.between(from: '2022-01-01', to: '2023-12-31'),
+      start_time: Faker::Date.between(from: '2022-01-01', to: '2022-12-31'),
+      end_time: Faker::Date.between(from: '2023-01-01', to: '2023-12-31')
+  )
+end

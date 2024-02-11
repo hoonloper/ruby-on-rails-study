2588413b372f57f5f5c7a4f0a4a3f0f09570099e
hoonloper <yonghoonj98@gmail.com>
2024-02-11 13:08:21 +0900

    Test: 티켓 테스트 코드 수정

--- ticketing/spec/models/ticket_spec.rb
+++ ticketing/spec/models/ticket_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe Ticket, type: :model do
   it { should belong_to(:screening) }
 
-  it { should validate_presence_of(:username) }
+  it 'validates uniqueness of username' do
+    # Theater와 Movie를 먼저 생성
+    theater = create(:theater)
+    movie = create(:movie)
+
+    # Screening을 생성할 때 생성한 Theater와 Movie를 지정
+    screening = create(:screening, theater: theater, movie: movie)
+
+    expect(build(:ticket, screening: screening)).to validate_uniqueness_of(:username)
+  end
 end

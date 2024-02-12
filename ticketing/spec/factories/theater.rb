fecee614c4e1324b9a31806c7ef7aaa12d66aa60
hoonloper <yonghoonj98@gmail.com>
2024-02-12 20:29:48 +0900

    Feat: theater faker digits 2자리로 변경

--- ticketing/spec/factories/theater.rb
+++ ticketing/spec/factories/theater.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :theater do
     name { Faker::Lorem.name }
-    max_audience_count { Faker::Number.number(digits: 5) }
+    max_audience_count { Faker::Number.number(digits: 2) }
   end
 end

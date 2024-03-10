4b2751f95bd59fd8c79d2e7d6a5ec57d142dd160
hoonloper <yonghoonj98@gmail.com>
2024-03-10 14:17:00 +0900

    Test: ActiveRecord 동적 변환 테스트 추가

--- deep-diving/spec/models/number_spec.rb
+++ deep-diving/spec/models/number_spec.rb
@@ -1,32 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Number, type: :model do
-  # it { should validate_presence_of(:integer_num) }
-  # it { should validate_presence_of(:float_num) }
+  it { should validate_presence_of(:integer_num) }
+  it { should validate_presence_of(:float_num) }
 
-  describe 'creating a number' do
-    it 'successfully creates a new number with valid attributes' do
-      number = Number.create(float_num: 1.6, integer_num: 1)
+  it '문자열 실수가 정수 컬럼에 저장된다면?' do
+    number = Number.create!(float_num: 1.6, integer_num: '1.6')
 
-      expect(number).to be_persisted
-      expect(number.float_num).to eq(1.6)
-      expect(number.integer_num).to eq(1)
-    end
+    expect(number.reload.integer_num).to eq(1)
   end
 
-  describe 'finding a number' do
-    let!(:number) { Number.create(float_num: 1.6, integer_num: 1) }
+  it '문자열 정수가 실수 컬럼에 저장된다면?' do
+    number = Number.create!(float_num: '1', integer_num: 1)
 
-    it 'finds a number by id' do
-      found_user = Number.find(number.id)
+    expect(number.reload.float_num).to eq(1)
+  end
 
-      expect(found_user).to eq(number)
+  describe '예상되는 케이스' do
+    describe '정수 컬럼에 실수 저장' do
+      it '실수를 정수 컬럼에 저장할 때 쿼리로 변환 과정중 타입 에러를 발생시킨다.' do
+        expect { Number.create!(float_num: 1.6, integer_num: 1.6) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+
+      it '실수가 정수 컬럼에 저장되지만 소수점은 모두 잘리고 저장된다. (1.6 -> 1)' do
+        number = Number.create!(float_num: 1.6, integer_num: 1.6)
+
+        expect(number.reload.integer_num).to eq(1)
+      end
+
+      it '실수가 정수 컬럼에 저장되지만 반올림되어 저장된다. (1.6 -> 2)' do
+        number = Number.create!(float_num: 1.6, integer_num: 1.6)
+
+        expect(number.reload.integer_num).to eq(2)
+      end
     end
+    describe '실수 컬럼에 정수 저장' do
+      it '정수를 실수 컬럼에 저장할 때 쿼리로 변환 과정중 타입 에러를 발생시킨다.' do
+        expect { Number.create!(float_num: 1, integer_num: 1) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
 
-    it 'finds a number by integer_num' do
-      found_user = Number.find_by(integer_num: 1)
+      it '정수가 실수 컬럼에 저장되지만 소수점을 적용한 후 저장된다. (1 -> 1.0)' do
+        number = Number.create!(float_num: 1, integer_num: 1)
 
-      expect(found_user).to eq(number)
+        expect(number.reload.float_num).to eq(1.0)
+      end
     end
   end
 end

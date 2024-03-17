a1d06a73c8b3b9d568b203a2cd990dc7070d8a20
hoonloper <yonghoonj98@gmail.com>
2024-03-17 15:08:34 +0900

    Feat: ActiveRecord에서 지원하는 cache_key 사용 예제 추가

--- redis-practice/app/helpers/redis_cache_helper.rb
+++ redis-practice/app/helpers/redis_cache_helper.rb
@@ -6,7 +6,10 @@ module RedisCacheHelper
     # posts_json = Rails.cache.fetch('posts', expires_in: 7.minutes, race_condition_ttl: 30.seconds) do
     #   posts.to_json
     # end
-    posts_json = Rails.cache.fetch(Post.cache_post_index, expires_in: 7.minutes, race_condition_ttl: 3) do
+    # posts_json = Rails.cache.fetch(Post.cache_post_index, expires_in: 7.minutes, race_condition_ttl: 3) do
+    #   posts.to_json
+    # end
+    posts_json = Rails.cache.fetch(posts.cache_key, expires_in: 7.minutes, race_condition_ttl: 3) do
       posts.to_json
     end
 
